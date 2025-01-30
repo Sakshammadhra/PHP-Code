b16844717fe8d55a456b7b8c5e38641962bb734e
Sakshammadhra <saksham.madhra@gmail.com>
2025-01-30 12:45:54 +0530

    Add files via upload

---
+++ 25.php
@@ -0,0 +1,8 @@
+WAP to get php vesion
+<br>
+<?php
+
+	echo 'Current PHP version: ' . phpversion();
+	echo "<br>";
+    echo "This Program is written by Saksham 0221BCA164"
+?>
---
+++ 9.php
@@ -0,0 +1,9 @@
+WAP to demonstrate resource types in php
+<?php
+
+    // Open a file for reading
+    $handle = fopen("note.txt", "r");
+    var_dump($handle);
+    echo "<br>";
+    echo "This Program is written by Saksham 0221BCA164"
+?>
